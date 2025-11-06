58ef17254b32fefee3cdea7d4a86d71cf962deb9
Mohammad Hizzani <moh.hizzani@gmail.com>
2025-11-06 12:19:13 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.56

---
+++ src/KSATGenerators.jl
@@ -0,0 +1,5 @@
+module KSATGenerators
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using KSATGenerators
+using Test
+
+@testset "KSATGenerators.jl" begin
+    # Write your tests here.
+end
